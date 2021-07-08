0ce7773785a194446c63e591ca89e9631169fcd8
Troza <trozah@gmail.com>
2021-07-08 14:09:32 -0500

    Contact Form Backend

--- server.py
+++ server.py
@@ -1,19 +1,27 @@
-import os
-
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, url_for, request, redirect
 
 app = Flask(__name__)
 print(__name__)
 
 @app.route('/')
-def hello_world():
+def my_home():
     return render_template('./index.html')
 
-@app.route('/about.html')
-def about():
-    return render_template('./about.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+def write_to_file(data):
+    with open('database.txt', mode ='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email}, {subject}, {message}')
 
-
-@app.route('/blog/2020/dogs')
-def blog2():
-    return 'this is my dog'
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'Something went wrong try again!'
